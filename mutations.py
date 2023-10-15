96bde12aeaec787147041726d8ea6f1e1cdd5c5a
George Harris <george.s.harris@gmail.com>
2023-10-15 13:28:36 +0100

    added BitStringStyleMutator to mutations.py

--- mutations.py
+++ mutations.py
@@ -9,3 +9,16 @@ class BaseMutator:
 
     def apply(self, tower_to_mutate: Tower):
         pass
+
+
+class BitStringStyleMutator(BaseMutator):
+    def apply(self, tower_to_mutate: Tower):
+        probability_to_mutate = 1/len(tower_to_mutate)
+        for block_i in range(len(tower_to_mutate)):
+            if np.random.binomial(n=1, p=probability_to_mutate) == 1:
+                if np.random.binomial(n=1, p=probability_to_mutate) == 1:
+                    tower_to_mutate.rotate_right(position=block_i)
+                else:
+                    tower_to_mutate.rotate_left(position=block_i)
+        return tower_to_mutate
+    
