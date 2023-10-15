3c42cd6637128121521ca33658ef2eb2d504716d
George Harris <george.s.harris@gmail.com>
2023-10-15 13:13:51 +0100

    removed SingularSwapper from mutations.py

--- mutations.py
+++ mutations.py
@@ -9,18 +9,3 @@ class BaseMutator:
 
     def apply(self, tower_to_mutate: Tower):
         pass
-
-
-class SingularSwapper(BaseMutator):
-    def apply(self, tower_to_mutate: Tower):
-        new_towers = []
-        blocks_to_rotate = np.random.randint(low=0, high=10, size=self.number_of_mutations)
-        direction = np.random.randint(low=0, high=2, size=self.number_of_mutations)
-
-        for i in range(self.number_of_mutations):
-
-            if direction[i] == 1:
-                new_towers.append(tower_to_mutate.rotate_left(blocks_to_rotate[i]))
-            else:
-                new_towers.append(tower_to_mutate.rotate_right(blocks_to_rotate[i]))
-        return new_towers
